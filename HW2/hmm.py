b74b32bb4d4bb13774eff55f5769ae504441017b
ZahraDehghani99 <zdehghanitafti78@gmail.com>
2022-04-23 21:15:07 +0430

    Added baum-welch algorithm for multiple sequences.

--- HW2/hmm.py
+++ HW2/hmm.py
@@ -1,101 +1,121 @@
-# implement hmm model
 import numpy as np
-# O = input columns : 51 in this case
-mapping = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5}
+from numba import jit
 
-# def forward(O, A, B, initial_probability):
-#     T = len(O) # time steps
-#     N = A.shape[0] # number of states
-#     alpha = np.zeros((N, T))
-#     for state in range(0, N):
-#         alpha[state, 0] = initial_probability[0][state] * B[mapping.get(O[0]), state] 
-#         # alpha[state, 0] = initial_probability[0][state] 
-#     for time in range(1, T):
-#         for state in range(0, N):
-#             alpha[state, time] = alpha[:, time-1].dot(A[state, :] * B[mapping.get(O[time]), state]) 
-#     termination_step =  np.sum(alpha[:, -1], axis=0)       
-#     return alpha, termination_step   
+def mapping_observation_multiple_seq(observ_lst):
+  observation = np.zeros((len(observ_lst), len(observ_lst[0])))
+  observation = np.asarray(observation, dtype=np.int8)
+  mapping = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5}
+  for i in range(len(observ_lst)): 
+    for j in range(len(observ_lst[0])):
+      observation[i, j] = int(mapping.get(observ_lst[i][j]))
+  return observation    
 
-def forward(O, A, B, initial_probability):
-    T = len(O) # time steps
-    N = A.shape[0] # number of states
-    B = B.T
-    alpha = np.zeros((T, N))
-    for state in range(0, N):
-        alpha[0, state] = initial_probability[0][state] * B[state, mapping.get(O[0])]
-    for time in range(1, T):
-        for state in range(0, N):
-            alpha[time, state] = alpha[time-1, :].dot(A[:, state] * B[state, mapping.get(O[time])]) 
-    termination_step =  np.sum(alpha[T-1, :])       
-    return alpha, termination_step      
+@jit(nopython=True)
+def forward(V, a, b, initial_distribution):
+    b = b.T
+    T = V.shape[1]
+    alpha = np.zeros((T, a.shape[0]))
+    alpha[0, :] = initial_distribution * b[:, V[0]]
 
-def backward(O, A, B, initial_probability):
-    T = len(O)
-    N = A.shape[0]
-    beta = np.zeros((T, N))
-    for state in range(0, N):
-        beta[-1, state] = 1
-    for time in range(T-2, -1, -1):
-        for state in range(0, N):
-            # beta[time, state] = (A[state, :] * B[mapping.get(O[time+1]),:]).dot(beta[:, time+1]) 
-            beta[time, state] = (beta[time+1, :]*B[:, mapping.get(O[time+1])]).dot(A[state, :] )       
-    termination_step = np.sum(((beta[0, :])*(initial_probability[0]).dot(B[:, mapping.get(O[0])])))   
-    return beta, termination_step   
+    for t in range(1, V.shape[0]):
+        for j in range(a.shape[0]):
+            alpha[t, j] = alpha[t - 1].dot(a[:, j]) * b[j, V[t]]
+    return alpha
 
-def baum_welch(O, a, b, initial_distribution, n_iter=100):
-    M = a.shape[0]
-    T = len(O)
- 
-    for n in range(n_iter):
-        alpha = forward(O, a, b, initial_distribution)
-        beta = backward(O, a, b)
- 
-        xi = np.zeros((M, M, T - 1))
-        for t in range(T - 1):
-            denominator = np.dot(np.dot(alpha[t, :].T, a) * b[:, mapping.get(O[t + 1])].T, beta[t + 1, :])
-            for i in range(M):
-                numerator = alpha[t, i] * a[i, :] * b[:, mapping.get(O[t + 1])].T * beta[t + 1, :].T
-                xi[i, :, t] = numerator / denominator
- 
-        gamma = np.sum(xi, axis=1)
-        a = np.sum(xi, 2) / np.sum(gamma, axis=1).reshape((-1, 1))
- 
-        # Add additional T'th element in gamma
-        gamma = np.hstack((gamma, np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
- 
-        K = b.shape[1]
-        denominator = np.sum(gamma, axis=1)
-        for l in range(K):
-            b[:, l] = np.sum(gamma[:, mapping.get(O) == l], axis=1)
- 
-        b = np.divide(b, denominator.reshape((-1, 1)))
- 
-    return {"a":a, "b":b}
 
-# def forward(V, a, b, initial_distribution):
-#     alpha = np.zeros((V.shape[0], a.shape[0]))
-#     alpha[0, :] = initial_distribution * b[:, V[0]]
+@jit(nopython=True) 
+def forward_multiple_seq(V, a, b, initial_distribution):
+  b = b.T
+  R, T = V.shape[0], V.shape[1]
+  N = a.shape[0]
+  alpha = np.zeros((R, T, a.shape[0]))
+  for r in range(R):
+    alpha[r, 0, :] = initial_distribution * b[:, V[r, 0]]
+  
+  for r in range(R):
+    for t in range(1, T):
+      for j in range(a.shape[0]):
+        alpha[r, t, j] = alpha[r, t - 1, :].dot(a[:, j]) * b[j, V[r,t]]
+  return alpha
  
-#     for t in range(1, V.shape[0]):
-#         for j in range(a.shape[0]):
-#             # Matrix Computation Steps
-#             #                  ((1x2) . (1x2))      *     (1)
-#             #                        (1)            *     (1)
-#             alpha[t, j] = alpha[t - 1].dot(a[:, j]) * b[j, V[t]]
-#     termination_step =  np.sum(alpha[-1, :], axis=1) 
-#     return alpha, termination_step
+
+@jit(nopython=True)
+def backward_multiple_seq(V, a, b):
+  b = b.T
+  R, T = V.shape[0], V.shape[1]
+  beta = np.zeros((R, T, a.shape[0]))
+  # setting beta(T) = 1
+  for r in range(R):
+    beta[r, T-1, :] = np.ones((a.shape[0]))
+
+  # Loop in backward way from T-1 to
+  # Due to python indexing the actual loop will be T-2 to 0
+  for r in range(R):
+    for t in range(T-2, -1, -1):
+      for j in range(a.shape[0]):
+        beta[r, t, j] = (beta[r, t + 1,:] * b[:, V[r, t+1]]).dot(a[j, :])
+  return beta
  
- 
-# def backward(V, a, b, initial_probability):
-#     beta = np.zeros((V.shape[0], a.shape[0]))
- 
-#     # setting beta(T) = 1
-#     beta[V.shape[0] - 1] = np.ones((a.shape[0]))
- 
-#     # Loop in backward way from T-1 to
-#     # Due to python indexing the actual loop will be T-2 to 0
-#     for t in range(V.shape[0] - 2, -1, -1):
-#         for j in range(a.shape[0]):
-#             beta[t, j] = (beta[t + 1] * b[:, V[t + 1]]).dot(a[j, :])
-#     termination_step = np.sum(((beta[:, 0])*(initial_probability[0]).dot(b[O[0], :])), axis=0)
-#     return beta, termination_step
+
+@jit(nopython=True)
+def baum_welch_multiple_seq(V, a, b, initial_distribution, n_iter=100):
+  N = a.shape[0]
+  R, T = V.shape[0], V.shape[1]
+  b = b.T
+
+  for n in range(n_iter):
+      alpha = forward_multiple_seq(V, a, b, initial_distribution)
+      beta = backward_multiple_seq(V, a, b)
+
+      xi = np.zeros((R, N, N, T-1))
+      for r in range(R):
+        denominator = 0
+        for t in range(T-1):
+          denominator = np.dot(np.dot(alpha[r, t, :].T, a) * b[:, V[r, t + 1]].T, beta[r, t + 1, :])
+          for i in range(N):
+              numerator = alpha[r, t, i] * a[i, :] * b[:, V[r, t + 1]].T * beta[r, t + 1, :].T
+              xi[r, i, :, t] = numerator / denominator
+
+      gamma = np.sum(xi, axis=2) # sum based on state axis 
+      a = np.sum(np.sum(xi, axis=3), axis=0) / np.sum(np.sum(gamma, axis=2), axis=0).reshape((-1, 1))
+      gamma_new = np.zeros((R, N, T))
+      # Add additional T'th element in gamma
+      for r in range(R):
+        gamma_new[r] = np.hstack((gamma[r], np.sum(xi[r, :, :, T-2], axis=1).reshape((-1, 1))))
+      K = b.shape[1]# unique states
+      denominator = np.sum(np.sum(gamma_new, axis=2), axis=0).reshape((-1, 1))
+
+      for l in range(K):
+        b[:, l] = np.sum(np.sum(gamma_new[:, :, V[r] == l], axis=2), axis=0)
+      b = np.divide(b, denominator.reshape((-1, 1)))
+      initial_distribution[:] = np.sum(gamma_new[:, :, 0], axis=0) / R 
+  return a, b, initial_distribution
+
+
+def likelihood(V, a, b, initial_distribution):
+    alpha = forward(V, a, b, initial_distribution)
+    probability = np.sum(alpha[-1, :])
+    return probability 
+
+
+def readFile(filename):
+    fileObj = open(filename, 'r', encoding='utf-8') # open the file in read mode
+    words = fileObj.read().splitlines() # puts the file into an array
+    fileObj.close()
+    return words
+
+
+def read_init_file(filename):
+    initial_pro, A, B = [], [], []
+    with open(filename, "r") as f:
+        mylist = [line.rstrip('\n') for line in f]
+        for i, line in enumerate(mylist):
+            if i in range(1, 2):
+                initial_pro.append([float(y) for y in line.split()])
+            if i in range(4, 10):  
+                A.append([float(y) for y in line.split()])
+            if i in range(12, 18):    
+                B.append([float(y) for y in line.split()])
+    initial_pro, A, B = np.asarray(initial_pro), np.asarray(A), np.asarray(B)
+    return initial_pro[0], A, B
+
